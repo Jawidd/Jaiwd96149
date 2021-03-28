612a972227fcd0bc31b97142a6d4910abb20f7ce
jawid00776 <81518758+jawid00776@users.noreply.github.com>
2021-03-28 18:47:44 +0430

    initial c

---
+++ src/test/java/hu/cs/ex2/studentmanagment/StudentManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cs.ex2.studentmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
